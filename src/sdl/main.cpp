b7a75a4ea724c5488df6c0a2c9a297e0864e4cad
a dinosaur <nick@a-dinosaur.com>
2019-10-12 01:00:02 +1100

    sdl: more sdl boilerplate & nonfunctional emulator

--- src/sdl/main.cpp
+++ src/sdl/main.cpp
@@ -1,5 +1,14 @@
-#include <cstdio>
 #include <SDL.h>
+#include <emulator.hpp>
+
+#include <cstdio>
+#include <vector>
+#include <memory>
+
+
+constexpr int DEFAULT_WIDTH  = 640;
+constexpr int DEFAULT_HEIGHT = 448;
+constexpr int BIOS_SIZE = 1024 * 1024 * 4;
 
 int main(int argc, char** argv)
 {
@@ -8,14 +17,33 @@ int main(int argc, char** argv)
 
     const int winpos = SDL_WINDOWPOS_UNDEFINED;
     SDL_Window* window = SDL_CreateWindow("DobieSDL",
-        winpos, winpos,
-        640, 448,
+        winpos, winpos, DEFAULT_WIDTH, DEFAULT_HEIGHT,
         SDL_WINDOW_RESIZABLE);
     if (!window)
     {
         SDL_Quit();
         return 1;
     }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    if (!renderer)
+    {
+    	SDL_Quit();
+    	return 1;
+    }
+
+    SDL_Texture* texture = SDL_CreateTexture(renderer,
+    	SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING,
+    	DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    if (!texture)
+    {
+    	SDL_Quit();
+    	return 1;
+    }
+
+    auto emu = std::unique_ptr<Emulator>(new Emulator());
+    emu->reset();
+    //emu->load_BIOS(bios_data.data());
 
     bool running = true;
     while (running)
@@ -28,8 +56,17 @@ int main(int argc, char** argv)
         }
 
         SDL_Delay(10); // Don't saturate
+        //emu->run();
+
+        //SDL_UpdateTexture(texture, rect, pixels, pitch);
+
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+        SDL_RenderPresent(renderer);
     }
 
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
