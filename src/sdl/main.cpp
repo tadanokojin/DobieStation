5eeb64fe2b56df474dcde181cabc4924cdf6b457
a dinosaur <nick@a-dinosaur.com>
2019-10-11 20:38:00 +1100

    sdl: skeleton window & event loop

--- src/sdl/main.cpp
+++ src/sdl/main.cpp
@@ -1,6 +1,36 @@
 #include <cstdio>
+#include <SDL.h>
 
 int main(int argc, char** argv)
 {
-	return 0;
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER))
+        return 1;
+
+    const int winpos = SDL_WINDOWPOS_UNDEFINED;
+    SDL_Window* window = SDL_CreateWindow("DobieSDL",
+        winpos, winpos,
+        640, 448,
+        SDL_WINDOW_RESIZABLE);
+    if (!window)
+    {
+        SDL_Quit();
+        return 1;
+    }
+
+    bool running = true;
+    while (running)
+    {
+        SDL_Event event;
+        while (SDL_PollEvent(&event) > 0)
+        {
+            if (event.type == SDL_QUIT)
+                running = false;
+        }
+
+        SDL_Delay(10); // Don't saturate
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
