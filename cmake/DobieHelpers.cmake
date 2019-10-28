ec8c50c734af20afc8269a8d234aaa72f3e6bc11
a dinosaur <nick@a-dinosaur.com>
2019-10-28 21:17:59 +1100

    cmake: vk: banish glslang wrangling to the pits of DobieHelper

--- cmake/DobieHelpers.cmake
+++ cmake/DobieHelpers.cmake
@@ -29,3 +29,38 @@ function(dobie_cxx_compile_options TARGET)
         $<$<CXX_COMPILER_ID:MSVC>:VC_EXTRALEAN>)
 
 endfunction()
+
+function(dobie_add_glslang SUBDIR)
+    if (NOT APPLE)
+
+        # override options
+        set(BUILD_TESTING OFF CACHE BOOL "Build the testing tree.")
+        set(BUILD_EXTERNAL OFF CACHE BOOL "Build external dependencies in /External")
+        set(SKIP_GLSLANG_INSTALL ON CACHE BOOL "Skip installation")
+        set(ENABLE_GLSLANG_BINARIES OFF CACHE BOOL "Builds glslangValidator and spirv-remap")
+        set(ENABLE_HLSL OFF CACHE BOOL "Enables HLSL input support")
+
+        # hide irrelevent variables from the GUI
+        mark_as_advanced(BUILD_EXTERNAL BUILD_SHARED_LIBS BUILD_TESTING
+            ENABLE_EMSCRIPTEN_ENVIRONMENT_NODE ENABLE_EMSCRIPTEN_SINGLE_FILE
+            ENABLE_GLSLANG_BINARIES ENABLE_GLSLANG_WEB_DEVEL ENABLE_GLSLANG_DEVEL
+            ENABLE_HLSL ENABLE_OPT ENABLE_PCH ENABLE_SPVREMAPPER
+            LLVM_USE_CRT_DEBUG LLVM_USE_CRT_MINSIZEREL LLVM_USE_CRT_RELEASE LLVM_USE_CRT_RELWITHDEBINFO
+            LLVM_USE_CRT_INSTALL
+            SKIP_GLSLANG_INSTALL USE_CCACHE)
+
+        add_subdirectory(${SUBDIR})
+
+        # place under the externals folder
+        set_property(TARGET glslang PROPERTY FOLDER External/glslang)
+        set_property(TARGET OGLCompiler PROPERTY FOLDER External/glslang)
+        set_property(TARGET OSDependent PROPERTY FOLDER External/glslang)
+        set_property(TARGET SPIRV PROPERTY FOLDER External/glslang)
+        set_property(TARGET SPVRemapper PROPERTY FOLDER External/glslang)
+
+        # add to externals namespace
+        add_library(Ext::glslang ALIAS glslang)
+        add_library(Ext::SPVRemapper ALIAS SPVRemapper)
+
+    endif()
+endfunction()
